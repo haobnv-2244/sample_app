e721472bdf4cc91e56c0138fbd7efaed14a58569
Nguyen Van Hao B <nguyen.van.haob@sun-asterisk.com>
2020-06-17 09:55:18 +0700

    chapter 7: sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = Settings.regex_valid_email
-
+  USERS_PARAMS = %i(name email password password_confirmation).freeze
   before_save :downcase_email
 
   validates :name, presence: true,
                    length: { maximum: Settings.users.user_name.maximum }
   validates :email, presence: true,
-                    length: { Settings.users.mail.maximum },
+                    length: { maximum: Settings.users.email.maximum },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: true
 
