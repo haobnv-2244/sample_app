3c28dad5ab02194dd9e499878f375c8d29b8564d
Nguyen Van Hao B <nguyen.van.haob@sun-asterisk.com>
2020-06-18 09:19:47 +0700

    chapter 8: log in

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,12 @@ class User < ApplicationRecord
                     uniqueness: true
 
   has_secure_password
+  class << self
+    def digest string
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+      BCrypt::Password.create string, cost: cost
+    end
+  end
 
   private
 
