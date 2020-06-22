67005e9725a2027358b0426c8ef0eeec1efa99e0
Nguyen Van Hao B <nguyen.van.haob@sun-asterisk.com>
2020-06-22 16:25:49 +0700

    chapter 11: Account activation

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,17 @@ module SampleApp
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.available_locales = [:en, :vi]
     config.i18n.default_locale = :vi
-  end
+
+    config.action_mailer.delivery_method = :smtp
+    # SMTP settings for gmail
+    config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: "gmail.com",
+      user_name: ENV['gmail_username'],
+      password: ENV['gmail_password'],
+      authentication: "plain",
+      enable_starttls_auto: true
+    }
+end
 end
